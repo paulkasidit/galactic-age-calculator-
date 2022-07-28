2df50195a3fc018b6dcc3d402d0c3ee994d2c023
Paul Kasidit <paulkasidit@gmail.com>
2022-07-28 15:47:40 -0700

    wrote tests for check input function

--- __tests__/age_calculator.tests.js
+++ __tests__/age_calculator.tests.js
@@ -8,6 +8,15 @@ describe('Age',() => {
     expect(age1.age).toEqual(24); 
   })  
 
+  test("invalid inputs should return false", () => {
+    const age1 = new Age('24'); 
+    expect(age1.checkInput()).toEqual(false);
+  })
+
+  test("valid inputs should return true", () => {
+    const age1 = new Age(24); 
+    expect(age1.checkInput()).toEqual(true);
+  })
   
 
 })
