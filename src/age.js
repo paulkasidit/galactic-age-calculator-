b28d41f02d06a0069daa959496587aa69ae61baf
Paul Kasidit <paulkasidit@gmail.com>
2022-07-28 15:48:27 -0700

    succesfully pass test for check input function in age class

--- src/age.js
+++ src/age.js
@@ -3,4 +3,16 @@ export default class Age {
   constructor(age) {
     this.age = age; 
   }
+
+  checkInput(){ 
+    if (Number.isInteger(this.age)){
+      return true;
+    } else {
+      return false; 
+    }
+  }
+
+  
+
+
 }
