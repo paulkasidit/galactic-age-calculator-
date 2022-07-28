f1d6736fe163af958ca3560909788f30e6b8d085
Paul Kasidit <paulkasidit@gmail.com>
2022-07-28 15:30:38 -0700

    wrote passing test age constructor function

--- __tests__/age_calculator.tests.js
+++ __tests__/age_calculator.tests.js
@@ -1,1 +1,13 @@
- 
+import { TestScheduler } from 'jest';
+import Age from '../src/age.js';
+
+describe('Age',() => {
+
+  test ("should return age as integer", () => {
+    const age1 = new Age(24); 
+    expect(age1.age).toEqual(24); 
+  })  
+
+  
+
+})
