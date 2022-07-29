340c9178b0d5171d9ea3961900fc0b3e366b8500
Paul Kasidit <paulkasidit@gmail.com>
2022-07-29 13:19:52 -0700

    remvoed tests and business logic for check input function as it is no longer needed.

--- __tests__/age_calculator.tests.js
+++ __tests__/age_calculator.tests.js
@@ -8,21 +8,5 @@ describe('Age',() => {
     expect(age1.age).toEqual(24); 
   }); 
 
-  test("invalid inputs should return false", () => {
-    const age1 = new Age('24'); 
-    expect(age1.checkInput()).toEqual(false);
-  });
-
-  test("valid inputs should return true", () => {
-    const age1 = new Age(24); 
-    expect(age1.checkInput()).toEqual(true);
-  });
-  
-  test("should return age multiplied by .24", () => {
-    const age1 = new Age(24); 
-    expect(age1.mercuryAge()).toEqual(5.76);
-  });
-  
-
 });
 
--- src/age.js
+++ src/age.js
@@ -4,23 +4,5 @@ export default class Age {
     this.age = age; 
   }
 
-  checkInput(){ 
-    if (Number.isInteger(this.age)){
-      return true;
-    } else {
-      return false; 
-    }
-  }
 
-  mercuryAge(){
-    let age = this.age; 
-
-    if (this.checkInput(age) === true){
-      return this.age * .24; 
-    } else if (this.checkInput(age) === true) {
-      this.age = "invalid input"; 
-    } else {
-      return this.age;
-    }
-}
 }
