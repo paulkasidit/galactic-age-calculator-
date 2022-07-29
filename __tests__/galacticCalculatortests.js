f81e18b74acd8a231ecbf88ec6798f9bdbc975ca
Paul Kasidit <paulkasidit@gmail.com>
2022-07-29 13:25:10 -0700

    renamed class from age to galacticCalculator

---
+++ __tests__/galacticCalculatortests.js
@@ -0,0 +1,17 @@
+import { TestScheduler } from 'jest';
+import galacticCalculator from '../src/galacticCalculator.js';
+
+describe('galacticCalculator',() => {
+
+  test ("should return age as integer", () => {
+    const age1 = new Age(24); 
+    expect(galacticCalculator.age).toEqual(24); 
+  }); 
+
+  test ("should return life expectancy as integer", () = {
+    const age1 = new Age(24, 85); 
+    expect(galacticCalculator.lifeExpectancy).toEqual(85); 
+  });
+
+});
+
